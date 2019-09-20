aebb1d704ef705e44c346db28cbc670965361ba2
itsaiub <itsaiub@gmail.com>
2019-09-20 16:55:11 +0600

    admin customization

--- listings/admin.py
+++ listings/admin.py
@@ -3,4 +3,15 @@ from . import models
 # Register your models here.
 
 
-admin.site.register(models.Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published',
+                    'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'address',
+                     'city', 'state', 'zipcode', 'price')
+    list_per_page = 25
+
+
+admin.site.register(models.Listing, ListingAdmin)
--- realtors/admin.py
+++ realtors/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
-admin.site.register(models.Realtor)
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', )
+    list_per_page = 20
+
+
+admin.site.register(models.Realtor, RealtorAdmin)
