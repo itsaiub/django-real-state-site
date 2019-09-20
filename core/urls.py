1f278da3391c36689758eb83860e10208859e524
itsaiub <itsaiub@gmail.com>
2019-09-20 16:28:06 +0600

    settings for Media FIle

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls', namespace='pages')),
     path('listing/', include('listings.urls', namespace='listings')),
     path('realtors/', include('realtors.urls', namespace='realtors')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
