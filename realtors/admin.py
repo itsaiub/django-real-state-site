42f255bf2854cee8a5426db5f35ee1334d3965b6
itsaiub <itsaiub@gmail.com>
2019-09-20 23:36:59 +0600

    home and about page dynamic

--- realtors/admin.py
+++ realtors/admin.py
@@ -4,9 +4,10 @@ from . import models
 
 
 class RealtorAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display = ('id', 'name', 'email', 'is_mvp', 'hire_date')
     list_display_links = ('id', 'name')
     search_fields = ('name', )
+    list_editable = ('is_mvp',)
     list_per_page = 20
 
 
