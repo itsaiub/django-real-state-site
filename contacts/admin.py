2bc78e16f8657995e2a14261dacecf702314df58
itsaiub <itsaiub@gmail.com>
2019-09-21 14:08:53 +0600

    contact form submission

--- contacts/admin.py
+++ contacts/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
-admin.site.register(models.Contact)
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'listing', 'email', 'contact_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'email', 'listing')
+    list_per_page = 10
+
+
+admin.site.register(models.Contact, ContactAdmin)
--- contacts/urls.py
+++ contacts/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'contacts'
 
 urlpatterns = [
-
+    path('contact', views.contact, name='contact')
 ]
